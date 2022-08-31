dbb6293d6112c7b72d849df51d0a4233b8792d24
LHardwick-git <77888938+LHardwick-git@users.noreply.github.com>
2022-08-31 15:05:48 +0100

    Added checks and service files

---
+++ dbus-i2c/check-i2c.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+if ! pgrep -f "dbus-i2c.py" >/dev/null; then
+    echo "process not found"
+  else
+    echo "process running"
+  fi
+#    exit 0
+#fi
+
+#if ! test -e /dev/gpio/digital_input_1/edge; then
+#    flags=--poll=poll
+#fi
+
+#exec $(dirname $0)/dbus-i2c.py
